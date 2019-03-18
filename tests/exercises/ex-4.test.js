00747ac13fb48285e96fffbcb71234b5e3f5555f
hunter-aw <hunteraweinsheink@gmail.com>
2019-03-18 15:47:40 +0200

    finished ex 3 and 4 tests

--- tests/exercises/ex-4.test.js
+++ tests/exercises/ex-4.test.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import assert, { notDeepEqual } from 'assert';
 import App from '../../src/App';
-import renderer from 'react-test-renderer';
 import { mount, render, shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { wrap } from 'module';
 import store from '../../src/stores/groceryStore'
-import { isObservable, isObservableProp } from 'mobx'
 
 configure({ adapter: new Adapter() });
 
@@ -23,10 +18,8 @@ describe("exercise 3", () => {
     })
     it('delete button should be rendered per each list item and work on click', () => {
         const wrapper = mount(<App store = {store}/>)
-        store.addItem("test") // does not save the item in the store? async?
         let selected = wrapper.find(".listItem").first().props().value
         expect(selected).toBe('Potatoes')
-        // let location = wrapper.find('.location').first().html()
         wrapper.find('.deleteButton').first().simulate('click')
         expect(wrapper.find(".listItem").first().props().value).not.toBe(selected)
     })
